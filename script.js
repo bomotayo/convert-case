b2a0bcf137d28e715a0798a99eebafdb996a1c57
bomotayo <omotayobabatundee@gmail.com>
2021-03-29 12:12:39 -0400

    fixed vowel and consonant case

--- script.js
+++ script.js
@@ -1,107 +1,104 @@
 //vars created from button IDs
-let container = document.querySelector('.container');
-let upperBtn = document.getElementById('upperBtn');
-let lowerBtn = document.getElementById('lowerBtn');
-let spinalBtn = document.getElementById('spinalBtn');
-let snakeBtn = document.getElementById('snakeBtn');
-let camelBtn = document.getElementById('camelBtn');
-let pascalBtn = document.getElementById('pascalBtn');
-let titleBtn = document.getElementById('titleBtn');
-let vowelBtn = document.getElementById('vowelBtn');
-let consoBtn = document.getElementById('consoBtn');
+let container = document.querySelector(".container");
+let upperBtn = document.getElementById("upperBtn");
+let lowerBtn = document.getElementById("lowerBtn");
+let spinalBtn = document.getElementById("spinalBtn");
+let snakeBtn = document.getElementById("snakeBtn");
+let camelBtn = document.getElementById("camelBtn");
+let pascalBtn = document.getElementById("pascalBtn");
+let titleBtn = document.getElementById("titleBtn");
+let vowelBtn = document.getElementById("vowelBtn");
+let consoBtn = document.getElementById("consoBtn");
 
+let clrBtn = document.getElementById("clrBtn");
 
-
-let clrBtn = document.getElementById('clrBtn');
-
-let text = document.getElementById('conBox');
+let text = document.getElementById("conBox");
 let regex = /[^a-zA-Z0-9]|\s+/g;
-let regexVow = /[aeiou]/g;
-
+let regexVow = /[aeiuo]+/g;
 
 // Event Listeners
-upperBtn.addEventListener('click', () => {
+upperBtn.addEventListener("click", () => {
   text.value = text.value.toUpperCase();
 });
 
-lowerBtn.addEventListener('click', () => {
+lowerBtn.addEventListener("click", () => {
   text.value = text.value.toLowerCase();
 });
 
-spinalBtn.addEventListener('click', () => {
-  text.value = text.value.replace(/([a-z])([A-Z])/g, "$1 $2");
-  text.value = text.value.replace(regex,'-').toLowerCase();
+spinalBtn.addEventListener("click", () => {
+  text.value = text.value.toLowerCase().replace(/([a-z])([A-Z])/g, "$1 $2");
+  text.value = text.value.replace(regex, "-").toLowerCase();
 });
 
-snakeBtn.addEventListener('click', () => {
-  text.value = text.value.replace(/([a-z])([A-Z])/g, "$1 $2");
-  text.value = text.value.replace(regex,'_').toLowerCase();
+snakeBtn.addEventListener("click", () => {
+  text.value = text.value.toLowerCase().replace(/([a-z])([A-Z])/g, "$1 $2");
+  text.value = text.value.replace(regex, "_").toLowerCase();
 });
 
-camelBtn.addEventListener('click', () => {
-  let arr = text.value.split('');
+camelBtn.addEventListener("click", () => {
+  let arr = text.value.toLowerCase().split("");
 
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i]===' '){
-      arr[i+1] = arr[i+1].toUpperCase();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === " ") {
+      arr[i + 1] = arr[i + 1].toUpperCase();
     }
   }
 
-  text.value = arr.join('').replace(/([a-zA-Z])(\s+)/g,'$1');
+  text.value = arr.join("").replace(/([a-zA-Z])(\s+)/g, "$1");
 });
 
-pascalBtn.addEventListener('click', () => {
-  let arr = text.value.split('');
+pascalBtn.addEventListener("click", () => {
+  let arr = text.value.toLowerCase().split("");
   arr[0] = arr[0].toUpperCase();
 
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i]===' '){
-      arr[i+1] = arr[i+1].toUpperCase();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === " ") {
+      arr[i + 1] = arr[i + 1].toUpperCase();
     }
   }
 
-  text.value = arr.join('').replace(/([a-zA-Z])(\s+)/g,'$1');
+  text.value = arr.join("").replace(/([a-zA-Z])(\s+)/g, "$1");
 });
 
-titleBtn.addEventListener('click', () => {
-  let arr = text.value.split('');
+titleBtn.addEventListener("click", () => {
+  let arr = text.value.toLowerCase().split("");
   arr[0] = arr[0].toUpperCase();
 
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i]===' '){
-      arr[i+1] = arr[i+1].toUpperCase();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === " ") {
+      arr[i + 1] = arr[i + 1].toUpperCase();
     }
   }
 
-  text.value = arr.join('');
+  text.value = arr.join("");
 });
 
-vowelBtn.addEventListener('click', () => {
-  let arr = text.value.split('');
+vowelBtn.addEventListener("click", () => {
+  let arr = text.value.toLowerCase().split("");
 
-  for(let i = 0; i < arr.length; i++){
-    if(regexVow.test(arr[i])){
+  for (let i = 0; i < arr.length; i++) {
+    if (regexVow.test(arr[i])) {
       arr[i] = arr[i].toUpperCase();
+      i++;
     }
   }
 
-  text.value = arr.join('');
+  text.value = arr.join("");
 });
 
-consoBtn.addEventListener('click', () => {
-  let arr = text.value.split('');
+consoBtn.addEventListener("click", () => {
+  let arr = text.value.toLowerCase().split("");
 
-  for(let i = 0; i < arr.length; i++){
-    if(!regexVow.test(arr[i])){
+  for (let i = 0; i < arr.length; i++) {
+    if (!regexVow.test(arr[i])) {
       arr[i] = arr[i].toUpperCase();
+      i++;
     }
   }
 
-  text.value = arr.join('');
+  text.value = arr.join("");
 });
 
-clrBtn.addEventListener('click', () => {
-  text.value = '';
+clrBtn.addEventListener("click", () => {
+  text.value = "";
 });
-
-
